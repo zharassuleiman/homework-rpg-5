64a8cb2e6311c95bb9935e516952ef5e54248c7e
Zharas Suleiman <zharas.suleiman@narxoz.kz>
2026-03-11 16:47:13 +0500

    finished

--- src/com/narxoz/rpg/facade/DungeonFacade.java
+++ src/com/narxoz/rpg/facade/DungeonFacade.java
@@ -15,15 +15,17 @@ public class DungeonFacade {
     }
 
     public AdventureResult runAdventure(HeroProfile hero, BossEnemy boss, AttackAction action) {
-        // TODO: Coordinate subsystem calls in a clean order.
-        // Suggested flow:
-        // 1) preparation
-        // 2) battle
-        // 3) reward
-        AdventureResult result = battleService.battle(hero, boss, action);
+        AdventureResult finalResult = new AdventureResult();
         String preparationSummary = preparationService.prepare(hero, boss, action);
-        result.addLine(preparationSummary);
-        result.setReward(rewardService.determineReward(result));
-        return result;
+        finalResult.addLine(preparationSummary);
+        AdventureResult battleResult = battleService.battle(hero, boss, action);
+        for (String line : battleResult.getLog()){
+            finalResult.addLine(line);
+        }
+        finalResult.setWinner(battleResult.getWinner());
+        finalResult.setRounds(battleResult.getRounds());
+        String reward = rewardService.determineReward(finalResult);
+        finalResult.setReward(reward);
+        return finalResult;
     }
 }
